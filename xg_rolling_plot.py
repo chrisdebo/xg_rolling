182e53f46577b9b4a710f65d584ff05cffee1468
pedoneA2 <christoph@debowiak.de>
2023-12-26 19:06:44 +0100

    - Combined scripts for data retrieval from fbref and xG rolling plot creation. - Encapsulated key functionalities into distinct functions. - Added get_team_data function for efficient team data fetching from fbref. - Implemented process_team_data function for processing the retrieved data, with a focus on xG rolling averages. - Streamlined the overall workflow and improved code organization. - Enhanced readability and maintainability of the code.

--- xg_rolling_plot.py
+++ xg_rolling_plot.py
@@ -7,17 +7,40 @@ import pandas as pd
 from PIL import Image
 from highlight_text import fig_text
 
-# Function to get today's date
-def get_today_date():
-    return datetime.datetime.now().strftime("%Y%m%d")
+# Dictionary with team information
+teams_info = {
+    'Union-Berlin': {'teamlogonumber': '8149', 'color1': '#ED1C24', 'color2': '#989898'},
+    # Add more teams here in the same format
+}
 
-# Function to create the team plot
-def create_team_plot(teamname, date, teamlogonumber, fotmob_url, color1, color2):
-    today_date = get_today_date()
-    file_path = os.path.join('data', teamname + '_' + date + '.json')
-    df = pd.read_json(file_path)
+def get_team_data(teamname, save_as_json=False):
+    base_url = 'https://fbref.com/de/mannschaften/7a41008f/{}/spielprotokolle/c20/schedule/{}-Punkte-und-Eckdaten-Bundesliga'
 
+    # fbref table link for the 23-24 season
+    url_df_2324 = base_url.format('2023-2024', teamname)
+    df_2324 = pd.read_html(url_df_2324)[0]
+
+    # fbref table link for the 22-23 season
+    url_df_2223 = base_url.format('2022-2023', teamname)
+    df_2223 = pd.read_html(url_df_2223)[0]
+
+    # Combine both DataFrames
+    df = pd.concat([df_2223, df_2324], axis=0).reset_index(drop=True)
+
+    print('Data for ' + teamname + ' successfully loaded.')
+
+    # Optionally save as JSON in the 'data' subfolder
+    if save_as_json:
+        file_path = os.path.join('data', teamname + '.json')
+        os.makedirs(os.path.dirname(file_path), exist_ok=True) # Create directory if it doesn't exist
+        df.to_json(file_path, orient='records')
+        print('Data for ' + teamname + ' successfully saved as JSON.')
+    return df
+
+# Data processing function
+def process_team_data(df):
     games_index = 0
+    # Find the index of the first NaN value in the 'Ergebnis' column
     for value in df['Ergebnis']:
         if pd.notna(value):
             games_index += 1
@@ -30,14 +53,18 @@ def create_team_plot(teamname, date, teamlogonumber, fotmob_url, color1, color2)
     df = df.sort_values(by="Datum")
     df.index = df.index + 1
 
-    Y_for = df.xG.rename('value', inplace=True)
-    Y_ag = df.xGA.rename('value', inplace=True)
-    X = pd.Series(range(len(Y_for)))
-    X = X + 1
-    X.index = X.index + 1
+    Y_for = df['xG'].rolling(window=5, min_periods=0).mean()
+    Y_ag = df['xGA'].rolling(window=5, min_periods=0).mean()
+    X = pd.Series(range(1, len(Y_for) + 1))
 
-    Y_for = Y_for.rolling(window=5, min_periods=0).mean()
-    Y_ag = Y_ag.rolling(window=5, min_periods=0).mean()
+    print('Data successfully processed.')
+    return X, Y_for, Y_ag
+
+# Function to create the team plot
+def create_team_plot(teamname, X, Y_for, Y_ag, save_as_png=False):
+    teamlogonumber = teams_info[teamname]['teamlogonumber']
+    color1 = teams_info[teamname]['color1']
+    color2 = teams_info[teamname]['color2']
 
     fig, ax = plt.subplots(figsize=(5, 2.5), dpi=200, facecolor="#EFE9E6")
     ax.set_facecolor("#EFE9E6")
@@ -49,6 +76,7 @@ def create_team_plot(teamname, date, teamlogonumber, fotmob_url, color1, color2)
 
     line_1 = ax.plot(X, Y_for, color=color1, zorder=4)
     line_2 = ax.plot(X, Y_ag, color=color2, zorder=4)
+
     ax.set_ylim(0)
 
     ax.plot([34, 34], [ax.get_ylim()[0], ax.get_ylim()[1]], ls=":", lw=1.25, color="grey", zorder=2)
@@ -70,28 +98,26 @@ def create_team_plot(teamname, date, teamlogonumber, fotmob_url, color1, color2)
 
     ax.xaxis.set_major_locator(ticker.MultipleLocator(5))
     ax.yaxis.set_major_locator(ticker.MultipleLocator(0.5))
-    ax.set_ylim(0)
 
     fig_text(x=0.12, y=1.1, s=teamname.replace('-', ' '), color="black", weight="bold", size=10, annotationbbox_kw={"xycoords": "figure fraction"})
     fig_text(x=0.12, y=1.02, s="Expected goals <for> and <against> // 5-game average\nBundesliga Season 22/23 & 23/24", highlight_textprops=[{"color": color1, "weight": "bold"}, {"color": color2, "weight": "bold"}], color="black", size=6, annotationbbox_kw={"xycoords": "figure fraction"})
 
+    fotmob_url = "https://images.fotmob.com/image_resources/logo/teamlogo/"
     logo_ax = fig.add_axes([0.65, .9, 0.3, 0.3], zorder=1)
     club_icon = Image.open(urllib.request.urlopen(f"{fotmob_url}{teamlogonumber}.png"))
     logo_ax.imshow(club_icon)
     logo_ax.axis("off")
 
-    output_folder = 'plots'
-    output_filename = os.path.join(output_folder, f'{teamname}_{today_date}.png')
-
-    plt.savefig(output_filename, dpi=200, bbox_inches='tight')
+    # Save the plot as PNG in the 'plots' subfolder
+    if save_as_png:
+        output_folder = 'plots'
+        output_filename = os.path.join(output_folder, f'{teamname}.png')
+        plt.savefig(output_filename, dpi=200, bbox_inches='tight')
+        print('Plot successfully saved as PNG.')
     plt.show()
 
-# Example usage of the function
+# Example usage
 teamname = 'Union-Berlin'
-date = "20231226"
-teamlogonumber = '8149'
-fotmob_url = "https://images.fotmob.com/image_resources/logo/teamlogo/"
-color1 = "#ED1C24"  # Example color
-color2 = "#989898"  # Example color
-
-create_team_plot(teamname, date, teamlogonumber, fotmob_url, color1, color2)
+df = get_team_data(teamname, save_as_json=True)
+X, Y_for, Y_ag = process_team_data(df)
+create_team_plot(teamname, X, Y_for, Y_ag, save_as_png=True)
