9eb15d00c41ee7a5e80a334b30d419b469bd13aa
pedoneA2 <christoph@debowiak.de>
2023-12-27 09:46:02 +0100

    - added loop to process all teams in teams_info - bug fix for teams with relegation games - bug fix in get_team_data base_url - added fbref team id in team_info - improvement in process_team_data - added some teams in team_info - updated README.md

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ This repository contains a Python script for retrieving Bundesliga team data fro
 ## Scripts
 
 - `get_team_data`: Retrieves data for a specific team for the 2022-2023 and 2023-2024 Bundesliga seasons and optionally saves it as a JSON file.
-- `process_team_data`: Processes the retrieved data, focusing on calculating rolling averages for xG.
+- `process_team_data`: Processes the retrieved data, focusing on calculating 5 game rolling averages for xG.
 - `create_team_plot`: Generates a plot showing the rolling xG averages, with an option to save the plot as a PNG file.
 
 ## Usage
@@ -20,29 +20,38 @@ This repository contains a Python script for retrieving Bundesliga team data fro
    ```python
    df = get_team_data("Team-Name", save_as_json=True)
 
-   Replace `"Team-Name"` with the desired teams name.
+   # Replace `"Team-Name"` with the desired team's name.
 
 2. **Process Team Data**:
    ```python
    X, Y_for, Y_ag = process_team_data(df)
    
-   Takes the DataFrame returned from get_team_data.
+   # Takes the DataFrame returned from get_team_data.
 
 3. **Create Team Plot:**:
    ```python
    create_team_plot('Team-Name', X, Y_for, Y_ag, save_as_png=True)
    
-   Generates and displays a plot for the specified team. Set save_as_png to True to save the plot.
+   # Generates and displays a plot for the specified team. Set save_as_png to True to save the plot.
   
 ## Team Configuration
 Edit the teams_info dictionary in the script to add or modify team-specific information like logo number and color preferences.
 
-## Example
+## Example for a single team
 ```python
 teamname = 'Union-Berlin'
 df = get_team_data(teamname, save_as_json=True)
 X, Y_for, Y_ag = process_team_data(df)
 create_team_plot(teamname, X, Y_for, Y_ag, save_as_png=True)
 ```
+
+## Example for all teams
+```python  
+for team in teams_info:
+    df = get_team_data(team, save_as_json=True)
+    X, Y_for, Y_ag = process_team_data(df)
+    create_team_plot(team, X, Y_for, Y_ag, save_as_png=True)
+```
+
 ## Thanks
 This addition gives proper credit to sonofacorner.com for the help with visualization.
--- xg_rolling_plot.py
+++ xg_rolling_plot.py
@@ -9,19 +9,27 @@ from highlight_text import fig_text
 
 # Dictionary with team information
 teams_info = {
-    'Union-Berlin': {'teamlogonumber': '8149', 'color1': '#ED1C24', 'color2': '#989898'},
+    'Union-Berlin': {'fbref_team_id': '7a41008f', 'teamlogonumber': '8149', 'color1': '#EB1923', 'color2': '#A9A9A9'},
+    'Bayer-Leverkusen': {'fbref_team_id': 'c7a9f859', 'teamlogonumber': '8178', 'color1': '#E32221', 'color2': '#A9A9A9'},
+    'Bayern-Munich': {'fbref_team_id': '054efa67', 'teamlogonumber': '9823', 'color1': '#DC052D', 'color2': '#A9A9A9'},
+    'Dortmund': {'fbref_team_id': 'add600ae', 'teamlogonumber': '9789', 'color1': '#FFD700', 'color2': '#A9A9A9'},
+    'Stuttgart': {'fbref_team_id': '598bc722', 'teamlogonumber': '10269', 'color1': '#E32219', 'color2': '#A9A9A9'},
+    'Bochum': {'fbref_team_id': 'b42c6323', 'teamlogonumber': '9911', 'color1': '#005CA9', 'color2': '#A9A9A9'},
     # Add more teams here in the same format
 }
 
 def get_team_data(teamname, save_as_json=False):
-    base_url = 'https://fbref.com/de/mannschaften/7a41008f/{}/spielprotokolle/c20/schedule/{}-Punkte-und-Eckdaten-Bundesliga'
+    fbref = teams_info[teamname]['fbref_team_id']
 
     # fbref table link for the 23-24 season
-    url_df_2324 = base_url.format('2023-2024', teamname)
+    base_url = 'https://fbref.com/de/mannschaften/{}/{}/spielprotokolle/c20/schedule/{}-Punkte-und-Eckdaten-Bundesliga'
+
+    # fbref table link for the 23-24 season
+    url_df_2324 = base_url.format(fbref, '2023-2024', teamname)
     df_2324 = pd.read_html(url_df_2324)[0]
 
     # fbref table link for the 22-23 season
-    url_df_2223 = base_url.format('2022-2023', teamname)
+    url_df_2223 = base_url.format(fbref, '2022-2023', teamname)
     df_2223 = pd.read_html(url_df_2223)[0]
 
     # Combine both DataFrames
@@ -40,21 +48,21 @@ def get_team_data(teamname, save_as_json=False):
 # Data processing function
 def process_team_data(df):
     games_index = 0
-    # Find the index of the first NaN value in the 'Ergebnis' column
-    for value in df['Ergebnis']:
-        if pd.notna(value):
-            games_index += 1
-        else:
-            print('Reached NaN value at ' + str(games_index))
-            break
 
-    df.drop(df.index[games_index:], inplace=True)
+    for index, row in df.iterrows():
+        if pd.notna(row['xG']): # check if there is a xG value
+            games_index += 1
+        else: # delete the row where xG is NaN
+            df.drop(index, inplace=True)
+
     df['Datum'] = df['Datum'].apply(lambda x: pd.to_datetime(x, format='%d.%m.%Y'))
     df = df.sort_values(by="Datum")
     df.index = df.index + 1
 
     Y_for = df['xG'].rolling(window=5, min_periods=0).mean()
     Y_ag = df['xGA'].rolling(window=5, min_periods=0).mean()
+    # Y_for = df['xG']
+    # Y_ag = df['xGA']
     X = pd.Series(range(1, len(Y_for) + 1))
 
     print('Data successfully processed.')
@@ -103,7 +111,7 @@ def create_team_plot(teamname, X, Y_for, Y_ag, save_as_png=False):
     fig_text(x=0.12, y=1.02, s="Expected goals <for> and <against> // 5-game average\nBundesliga Season 22/23 & 23/24", highlight_textprops=[{"color": color1, "weight": "bold"}, {"color": color2, "weight": "bold"}], color="black", size=6, annotationbbox_kw={"xycoords": "figure fraction"})
 
     fotmob_url = "https://images.fotmob.com/image_resources/logo/teamlogo/"
-    logo_ax = fig.add_axes([0.65, .9, 0.3, 0.3], zorder=1)
+    logo_ax = fig.add_axes([0.65, .9, 0.2, 0.2], zorder=1)
     club_icon = Image.open(urllib.request.urlopen(f"{fotmob_url}{teamlogonumber}.png"))
     logo_ax.imshow(club_icon)
     logo_ax.axis("off")
@@ -113,11 +121,17 @@ def create_team_plot(teamname, X, Y_for, Y_ag, save_as_png=False):
         output_folder = 'plots'
         output_filename = os.path.join(output_folder, f'{teamname}.png')
         plt.savefig(output_filename, dpi=200, bbox_inches='tight')
-        print('Plot successfully saved as PNG.')
+        print(f'Plot for {teamname} successfully saved as PNG.')
     plt.show()
 
-# Example usage
-teamname = 'Union-Berlin'
-df = get_team_data(teamname, save_as_json=True)
-X, Y_for, Y_ag = process_team_data(df)
-create_team_plot(teamname, X, Y_for, Y_ag, save_as_png=True)
+# # Example usage for Union Berlin
+# teamname = 'Stuttgart'
+# df = get_team_data(teamname, save_as_json=True)
+# X, Y_for, Y_ag = process_team_data(df)
+# create_team_plot(teamname, X, Y_for, Y_ag, save_as_png=True)
+
+# Loop through all teams in the dictionary
+for teamname in teams_info.keys():
+    df = get_team_data(teamname, save_as_json=False)
+    X, Y_for, Y_ag = process_team_data(df)
+    create_team_plot(teamname, X, Y_for, Y_ag, save_as_png=True)
